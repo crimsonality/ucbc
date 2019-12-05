afe1daa44e97a143a3291a5039f08b86c18aa1f3
Crimsonality <nano10606@gmail.com>
2019-12-04 22:47:33 -0500

    test push

--- src/components/App.js
+++ src/components/App.js
@@ -13,7 +13,7 @@ class App extends Component {
         return (
             <div className="App">
               <header className="App-header">
-                Jiwoo Kim
+                Jiwoo Kim personal website
               </header>
             </div>
           )
